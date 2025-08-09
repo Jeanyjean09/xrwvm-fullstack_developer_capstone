b38d97128d8e85972dfbdbb38710e0035bd7126a
Jeanyjean09 <geromomeviljean@gmail.com>
2025-08-09 09:42:39 -0400

    log in save

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,16 +1,17 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
+from django.contrib.auth.views import LogoutView
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+    path(route='login', view=views.login_user, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     # path for dealer reviews view
 
     # path for add a review view
