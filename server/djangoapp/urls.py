1c6ede53d21a374cb3dc9921cb20be2ea2f10a04
Jeanyjean09 <geromomeviljean@gmail.com>
2025-08-11 10:00:32 -0400

    add cars

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register', views.registration, name='register'),  # <-- Registration route
-    # ... other paths ...
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,10 +3,28 @@ from django.contrib.auth import logout, login, authenticate
 from django.views.decorators.http import require_GET
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from .populate import initiate
 import logging
 import json
 
+# Import Car Models
+from .models import CarMake, CarModel
+
 logger = logging.getLogger(__name__)
+
+# =====================
+# Get Cars Method
+# =====================
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels": cars})
 
 @csrf_exempt
 def login_user(request):
